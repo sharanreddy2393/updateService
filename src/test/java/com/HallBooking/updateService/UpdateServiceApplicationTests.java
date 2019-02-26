666639b31f3169d11c79e10409343046d0c076f7
sharanreddy2393 <sharanreddy2393@gmail.com>
2019-02-26 17:20:53 +0530

    initial code setup

---
+++ src/test/java/com/HallBooking/updateService/UpdateServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.HallBooking.updateService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UpdateServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
