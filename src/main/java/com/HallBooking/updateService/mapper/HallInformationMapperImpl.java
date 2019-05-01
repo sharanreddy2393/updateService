c4b3766324c9513eff4c8790572abcc504403171
sharanreddy2393 <sharanreddy2393@gmail.com>
2019-05-02 00:07:09 +0530

    mapping for userinformation and hallinformation

--- src/main/java/com/HallBooking/updateService/mapper/HallInformationMapperImpl.java
+++ src/main/java/com/HallBooking/updateService/mapper/HallInformationMapperImpl.java
@@ -7,14 +7,14 @@ import com.HallBooking.common.Entity.HallInformation;
 import com.HallBooking.common.Entity.OwnerInformation;
 import com.HallBooking.common.Entity.UserInfromation;
 
-public class HallInformationMapperImpl implements HallInformationMapper{
+public class HallInformationMapperImpl implements HallInformationMapper {
 
 	private HallInformation hall;
 	private BookingInformation info;
-	
+
 	@Override
 	public HallInformation mapHallInformation(HallRequest hallInfo) {
-		hall= new HallInformation();
+		hall = new HallInformation();
 		hall.setHallName(hallInfo.getHallName());
 		hall.setMuncipalRegistration(hallInfo.getMuncipalRegistration());
 		hall.setHallDescription(hallInfo.getHallDescription());
@@ -30,24 +30,23 @@ public class HallInformationMapperImpl implements HallInformationMapper{
 		hall.setHallCapacity(hallInfo.getHallCapacity());
 		OwnerInformation oinfo = new OwnerInformation();
 		oinfo.setOwnerId(hallInfo.getOwnerid());
-		hall.setOwnerid(oinfo);
+		hall.setOwnerInformation(oinfo);
 		return hall;
 	}
-
 
 	@Override
 	public BookingInformation MapBookingInformation(BookingInformationDTO bookingInfo) {
 		info = new BookingInformation();
 		info.setAdvanceAmount(bookingInfo.getAdvanceAmount());
-		info.setFromDate(new java.sql.Date( bookingInfo.getFromDate().getTime()));
-		info.setToDate( new java.sql.Date(bookingInfo.getToDate().getTime()));
+		info.setFromDate(new java.sql.Date(bookingInfo.getFromDate().getTime()));
+		info.setToDate(new java.sql.Date(bookingInfo.getToDate().getTime()));
 		info.setNumOfDays(bookingInfo.getNumOfDays());
-		UserInfromation uinfo= new UserInfromation();
+		UserInfromation uinfo = new UserInfromation();
 		uinfo.setUserId(bookingInfo.getUserId());
-		info.setUserId(uinfo);
+		info.setUserInformation(uinfo);
 		HallInformation hinfo = new HallInformation();
 		hinfo.setHallId(bookingInfo.getHallId());
-		info.setHallId(hinfo);
+		info.setHallInformation(hinfo);
 		return info;
 	}
 
